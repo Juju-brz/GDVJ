758a96aecc8187dae9424e70e4a0469cd568880b
Juju <Julienbrouzes@hotmail.com>
2025-12-01 00:00:43 +0100

    its better

--- hudControl.gd
+++ hudControl.gd
@@ -1,18 +1,18 @@
 extends Control
 
-@export var ma_variable: float = 0.0
-@onready var slider : HSlider = $HSlider
-
-signal valeur_slider_modifiee(nouvelle_valeur)
-
-func _ready():
-	# Remplace "Slider" par le chemin correct
-	if slider:
-		slider.value = ma_variable
-		slider.connect("value_changed", _on_slider_value_changed)
-	else:
-		print("Erreur : Le nœud Slider n'a pas été trouvé !")
-
-func _on_slider_value_changed(valeur):
-	ma_variable = valeur
-	emit_signal("valeur_slider_modifiee", valeur)
+#@export var ma_variable: float = 0.0
+#@onready var slider : HSlider = $HSlider
+#
+#signal valeur_slider_modifiee(nouvelle_valeur)
+#
+#func _ready():
+	## Remplace "Slider" par le chemin correct
+	#if slider:
+		#slider.value = ma_variable
+		#slider.connect("value_changed", _on_slider_value_changed)
+	#else:
+		#print("Erreur : Le nœud Slider n'a pas été trouvé !")
+#
+#func _on_slider_value_changed(valeur):
+	#ma_variable = valeur
+	#emit_signal("valeur_slider_modifiee", valeur)
--- square.gd
+++ square.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
-const NB_DUPLICATES: int = 5
+#var NB_DUPLICATES: int = 1.0
 const SPACING: float = 50.0
+#var original_sprite = $Square  
 
 @export var angle = 0.0
 @onready var hud = $"/root/Node2D/HUD"  # Chemin absolu vers ton HUD
-func _ready():
+
+
+func transform():
 	var original_sprite = $Square  
 	if not original_sprite:
 		print("No Sprint2D found")
@@ -22,7 +25,27 @@ func _ready():
 		#)
 		angle = (i + 1) * SPACING
 		duplicated_sprite.position = Vector2(
-			original_sprite.position.x + cos(angle) * 100,
-			original_sprite.position.y + sin(angle) * 100
+			original_sprite.position.x + cos(angle) * 0,
+			original_sprite.position.y + sin(angle) * 0
 		)
 		duplicated_sprite.rotation = angle
+
+var nb_duplicates: float = 1.0
+var NB_DUPLICATES: int = 1
+
+var old_nb: float = NB_DUPLICATES
+
+func _ready() -> void:
+	NB_DUPLICATES = clamp(NB_DUPLICATES, 0, 5)
+
+
+func _process(delta: float) -> void:
+	nb_duplicates += delta *1.4               # float qui s'incrémente
+	NB_DUPLICATES = int(nb_duplicates)    # conversion propre2.0
+	print(NB_DUPLICATES)
+	if NB_DUPLICATES > old_nb:
+		print("La valeur augmente :", NB_DUPLICATES)
+		transform()
+
+	# Met à jour l’ancienne valeur pour la frame suivante
+	old_nb = NB_DUPLICATES
