77e52d253fe0db7a21770f7ece9af3aacf22760d
Juju <Julienbrouzes@hotmail.com>
2025-12-09 20:09:48 +0100

    just stop a lot of thing useless to code code

--- code_edit.gd
+++ code_edit.gd
@@ -9,29 +9,17 @@ func _ready():
 	var button = $Button
 	button.pressed.connect(_on_button_pressed)
 
-	print("ColorRect trouvé ?", colorRect != null)
-	if colorRect:
-		print("Matériau du ColorRect :", colorRect.material)
-	else:
-		print("Erreur : ColorRect non trouvé !")
 
 func _on_button_pressed():
 	var user_code = self.text
 	
-	if not colorRect:
-		print("ColorRect introuvable")
-		return
 
 	var mat = colorRect.material
 	if mat is ShaderMaterial:
 		var shader = mat.shader
 		if shader:
 			shader.code = user_code   # ✔️ Modifie directement le shader
-			print("Shader modifié !")
-		else:
-			print("Ce ShaderMaterial n'a pas de shader !")
-	else:
-		print("Le matériau n'est pas un ShaderMaterial !")
+
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("hide_Code"):
