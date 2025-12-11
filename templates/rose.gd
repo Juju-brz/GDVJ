3572e2de914d036f9dedb8e4d08cb2541adee82d
Juju <Julienbrouzes@hotmail.com>
2025-12-11 14:35:24 +0100

    add new template and upgrade persian template

--- templates/rose.gd
+++ templates/rose.gd
@@ -8,6 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	super._process(delta)
-	increment()
-	print(delta)
+	pass
