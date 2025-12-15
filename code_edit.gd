f0ca70e79672d07446ce201be26e982efd5ab00e
Juju <Julienbrouzes@hotmail.com>
2025-12-15 18:38:28 +0100

    okay I cant chang font size

--- code_edit.gd
+++ code_edit.gd
@@ -6,20 +6,10 @@ var shader_path = "res://shaders/shaderEmpty.gdshader"
 @onready var colorRect = get_node("../../ColorRect2")  # Remonte de deux niveaux
 
 func _ready():
-	var button = $Button
-	button.pressed.connect(_on_button_pressed)
+	var execute = $Execute
+	execute.pressed.connect(_on_execute_pressed)
 	if colorRect == null:
 		print("Error: colorRect is null.")
-
-func _on_button_pressed():
-	var user_code = self.text
-	
-
-	var mat = colorRect.material
-	if mat is ShaderMaterial:
-		var shader = mat.shader
-		if shader:
-			shader.code = user_code   # ✔️ Modifie directement le shader
 
 
 func _input(event: InputEvent) -> void:
@@ -28,4 +18,23 @@ func _input(event: InputEvent) -> void:
 		hide = !self.visible
 	
 	if Input.is_action_just_pressed("execute_shader"):
-		_on_button_pressed()
+		_on_execute_pressed()
+
+
+#func Fontlarge():
+	#var font = code
+	#if font:
+		#font.font_size = 32  # Modifie la taille du Font
+		#$CodeEdit.add_font_override("font", font)  # Applique le changement
+
+
+func _on_execute_pressed() -> void:
+	var user_code = self.text
+	
+
+	var mat = colorRect.material
+	if mat is ShaderMaterial:
+		var shader = mat.shader
+		if shader:
+			shader.code = user_code   # ✔️ Modifie directement le shader
+			
